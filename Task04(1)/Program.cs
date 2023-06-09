82ed688a9db34bb44ea4978e732c3a62c2f5e65b
Tatyana <tanipermiakova@yandex.ru>
2023-06-09 22:56:43 +0400

    add

---
+++ Task04(1)/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+
+Console.WriteLine("Введите число A "); 
+int numberA = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число B "); 
+int numberB = Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine("Введите число C "); 
+int numberC = Convert.ToInt32(Console.ReadLine()); 
+int max = numberA;  
+
+if (numberA > numberB) 
+{
+    Console.WriteLine($" max = {numberA} ");
+}
+else if (numberB > numberC)
+{
+    Console.WriteLine($"max = {numberB}");
+}
+else if (numberC > numberA) 
+{
+    Console.WriteLine($" max = {numberC} ");  
+}
---
+++ Task06/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да 
+
+Console.WriteLine("Введите целое число "); 
+int number = Convert.ToInt32(Console.ReadLine());  
+
+if (number % 2 ==0) 
+{
+    Console.WriteLine($"{number} -> да"); 
+}
+else 
+{
+    Console.WriteLine($"{number} -> нет");
+}
