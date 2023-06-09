8ebba68e57bb66b5ed560cd132139b68b0e93018
Tatyana <tanipermiakova@yandex.ru>
2023-06-09 23:01:30 +0400

    add

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// на входе число, на выходе квадрат 
+Console.WriteLine("Введите целое число  ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int square = number * number; 
+Console.WriteLine($"Квадрат числа {number} = {square}"); 
+
--- Task02(03)/Program.cs
+++ Task02(03)/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // a = 5; b = 7 -> max = 7
 
-Console.WriteLine("Введите значение А  "); 
+Console.WriteLine("Введите значение A  "); 
 int numberA = Convert.ToInt32(Console.ReadLine()); 
 Console.WriteLine("Введите значение B  "); 
 int numberB = Convert.ToInt32(Console.ReadLine()); 
