a74257a41e1c9b9be50145b5a0942afb05e929c9
Tatyana <tanipermiakova@yandex.ru>
2023-06-12 01:27:56 +0400

    add

---
+++ Task25/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 3, 5 -> 243 (3⁵)
+
+Console.WriteLine("Введите целое число A "); 
+int numberA = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите целое число B "); 
+int numberB = Convert.ToInt32(Console.ReadLine()); 
+int i = 1;
+
+if (numberB <= 0) Console.WriteLine("Введено ненатуральное число");
+else
+{
+    while (numberB !=0) 
+    {
+        i = i * numberA; 
+        numberB = numberB - 1;
+    }
+    Console.WriteLine($"Получившаяся экпонента числа {numberA} = {i}");
+}
+
+
+
+//double Exponent (double numA, double numB) 
+//{
+//    double expo = Math.Pow(numA, numB); 
+//}
+
+
+
+
+
+// В только натуральным - проверка. Используем метод
