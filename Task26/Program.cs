3bec1f5a922560663b7d106be59a2d7aa877be00
Tatyana <tanipermiakova@yandex.ru>
2023-06-19 01:38:19 +0400

    reverseArray

---
+++ Task26/Program.cs
@@ -0,0 +1,19 @@
+﻿// на входе число и выдает количество цифр в числе. 456 -- 3 
+
+Console.WriteLine("Введите целое число "); 
+int number = Convert.ToInt32(Console.ReadLine());  
+
+int count = CountNumber(number); 
+Console.WriteLine($"Количество цифр в числе {number} = {count}");
+
+
+int CountNumber(int num) 
+{
+    int i = 0;
+    while (num != 0)
+    {
+        num = num / 10; 
+        i++;
+    } 
+    return i; 
+}
