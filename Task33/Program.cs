c28cfca9e7942d74939b93cc32e7540f1a83c42b
Tatyana <tanipermiakova@yandex.ru>
2023-07-01 01:09:01 +0400

    про прямые и точку пересечения

---
+++ Task33/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте массив. Программа, которая определяет, присутствует ли заданное число в массиве.
+
+int[] CreateArrayRndInt (int size, int min, int max)              
+{
+    int[] arr = new int[size]; 
+    Random rnd = new Random(); 
+    for (int i = 0; i < size; i++)          //arr.Lenght
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}  
+
+void PrintArray (int[] arr, string sep)                
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]} {sep}");
+        else Console.Write($"{arr[i]}");
+    }
+}     
+
+bool NumberInArray (int num, int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Введите число элементов ");
+int elements = Convert.ToInt32(Console.ReadLine()); 
+int[] arr = new int [elements];    
+
+Console.WriteLine("Введите минимальное число диапазона "); 
+int minimum = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите максимальное число диапазона "); 
+int maximum = Convert.ToInt32(Console.ReadLine()); 
+int[] array = CreateArrayRndInt(elements, minimum, maximum);  //elements
+
+Console.Write("[");
+PrintArray(array, ","); 
+Console.Write("]"); 
+
+Console.WriteLine("Введите число, которое нужно проверить в массиве  ");
+int number = Convert.ToInt32(Console.ReadLine());  
+
+bool isExistNum = NumberInArray (number, array); 
+Console.WriteLine(isExistNum ? "число есть в массиве" : "Числа нет в массиве");
--- Task42/Program.cs
+++ Task42/Program.cs
@@ -1,6 +1,20 @@
 ﻿// программа преобразующая десятичное число в двоичное
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите целое число  "); 
+int number = Convert.ToInt32(Console.ReadLine()); 
 
+string result = DecToBun(number);
+Console.WriteLine(result); 
 
-// c 1/08/25
+string DecToBin(int num) 
+{
+    string res = string.Empty; 
+    while (num > 0) 
+    {
+        res = num % 2 + res;  // это в обратном порядке, а если в прямом порядке res= res + num %2
+        num = num / 2;
+    }
+    return res;
+}
+
+ 
