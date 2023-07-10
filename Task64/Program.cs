2c006e617b3ab8da03a087a3ba90b4e3b5bb3ba6
Tatyana <tanipermiakova@yandex.ru>
2023-07-10 13:00:11 +0400

    рекурсия, сумма чисел от М до Н

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+CheckNaturalNumbers(number);
+NaturalNumbers(number);
+
+bool CheckNaturalNumbers(int num) 
+{
+    return num % 10 == 0;
+}
+
+void NaturalNumbers (int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers (num - 1);
+}
+
