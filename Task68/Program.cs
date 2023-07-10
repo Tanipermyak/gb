d079f9d5b1363cb04e797de200c29bbfe2e6c984
Tatyana <tanipermiakova@yandex.ru>
2023-07-10 13:53:17 +0400

    рекурсия, число в степени другого

---
+++ Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+// 
+Console.Write("Задайте неотрицательное число m = ");
+int numberm = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Задайте неотрицательное число n = ");
+int numbern = Convert.ToInt32(Console.ReadLine()); 
+
+int functionAkkerman = FunctionAkkerman(numberm, numbern);
+Console.WriteLine($"Функция Аккермана {functionAkkerman}");
+
+
+int FunctionAkkerman(int numm,int numn)
+{
+    if (numm == 0) return numn + 1; 
+    else if (numn == 0) return FunctionAkkerman(numm -1, 1);
+    else return FunctionAkkerman(numm - 1, FunctionAkkerman(numm, numn - 1));
+}
+
+// знаю, что stack overflow, но не смогла найти ошибку
