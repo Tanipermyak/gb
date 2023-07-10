10780ee8d589fe8cb1219b558c9175ab18bedb46
Tatyana <tanipermiakova@yandex.ru>
2023-07-10 13:52:04 +0400

    функция Аккермана

---
+++ Task66/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+
+Console.WriteLine("Задайте натуральное значение M  ");
+int numberM = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Задайте натуральное значение N  ");
+int numberN = Convert.ToInt32(Console.ReadLine()); 
+NaturalNumbers(numberM, numberN);
+int sumNumbersMN = SumNumbersMN(numberM, numberN);
+Console.WriteLine($"Сумма натуральных элементов {numberM}; {numberN} равна {sumNumbersMN}"); 
+
+void NaturalNumbers (int numM, int numN) 
+{
+    if (numM > numN)
+    {
+        Console.Write($"{numM}  ");
+        NaturalNumbers (numM - 1, numN);
+    }
+    else if (numM < numN)
+    {
+        Console.Write($"{numM}  ");
+        NaturalNumbers (numM + 1, numN); 
+    } 
+    else Console.Write($"{numM}  ");
+}
+
+int SumNumbersMN (int numM, int numN)
+{
+    if (numM == 0) return numN * (numN + 1) / 2; 
+    else if (numN == 0) return numM * (numM + 1) / 2;
+    else if (numM == numN) return numM; 
+    else if (numM < numN) return numN +SumNumbersMN(numM, numN -1); 
+    else return numN + SumNumbersMN(numM, numN +1);
+}
+
+
+
+
+
+
+//void
